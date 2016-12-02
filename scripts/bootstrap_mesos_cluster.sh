6f5bdea1efd32183eadf31b8ca1661a195c5104d
OthmaneN <nahyl.othmane@gmail.com>
2016-12-02 09:28:07 +0100

    added some comments

--- scripts/bootstrap_mesos_cluster.sh
+++ scripts/bootstrap_mesos_cluster.sh
@@ -2,7 +2,10 @@
 #first node is the master
 NODES=('server-1' 'server-2' 'server-3' 'server-4')
 
+#Installation des dépendances
 function install_deps {
+    #Si on est entrain de configurer le master on ajoute son hostname aux arguments du script
+    #Le master requiert un peu plus de config, notamment pour spark
     if [[ $1 -eq ${NODES[0]} ]]; then
     	ssh -q -i ~/.ssh/xnet $1 "bash -s > /dev/null 2>&1" < ./install_node_deps.sh $1 &
     else
@@ -24,11 +27,11 @@ for node in "${NODES[@]}"
 		install_deps ${node}
 	done
 
-echo 'Configuration du master'
+echo 'Configuration du master mesos'
 configure_mesos_master ${NODES[0]}
 
 for slave in "${NODES[@]:1}"
 	do
-		echo "Configuration de l'esclave "${slave}
+		echo "Configuration de l'esclave mesos "${slave}
 		configure_mesos_slave ${slave}
 	done
