3dd622af4b604b97b71374997b04b31270defea0
OthmaneN <nahyl.othmane@gmail.com>
2016-12-03 15:14:14 +0100

    fixed error in ssh commans

--- scripts/run_mesos_master.sh
+++ scripts/run_mesos_master.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 MY_IP=$(cat /etc/my_ip)
-sudo mesos-master --ip=${MY_IP} --work_dir=/var/lib/mesos > /dev/null 2>&1
+sudo mesos-master --ip=${MY_IP} --work_dir=/var/lib/mesos
--- scripts/run_mesos_slave.sh
+++ scripts/run_mesos_slave.sh
@@ -2,4 +2,4 @@
 
 MY_IP=$(cat /etc/my_ip)
 MASTER=$1
-sudo mesos-slave --ip=${MY_IP} --master=${MASTER}:5050 --work_dir=/var/lib/mesos > /dev/null 2>&1 &
+sudo mesos-slave --ip=${MY_IP} --master=${MASTER}:5050 --work_dir=/var/lib/mesos
