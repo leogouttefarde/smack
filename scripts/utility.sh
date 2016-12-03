8876ae04eb3f96e63cc3330ba3d90baf5f7b2897
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2016-12-03 04:47:25 +0100

    Fixed remote_run issue

--- scripts/utility.sh
+++ scripts/utility.sh
@@ -7,18 +7,28 @@ HOSTS=/etc/hosts
 HOSTS_BACKUP=$HOSTS.old
 LOCAL_IP=127.0.1.1
 
-# Runs a script remotely & asynchronously.
+# Runs a remote script (asynchronous)
 # Usage : remote_run <server> <script>
 # Scripts must have the .sh extension
 remote_run()
+{
+  if [[ $# -ge 2 && "$2" = *.sh ]]; then
+    ssh -q ${SSH_OPTS} xnet@$1 "bash $2" &
+  fi
+}
+
+# Sends a script, then runs it remotely (asynchronous)
+# Usage : remote_run <server> <script>
+# Scripts must have the .sh extension
+send_run()
 {
   if [[ $# -ge 2 && -f "$2" && "$2" = *.sh ]]; then
     local SPATH=/tmp/$(basename $2)
     scp ${SSH_OPTS} $2 xnet@$1:$SPATH &>/dev/null
 
     # fixes weird var expand bug
-    local CMD=$(echo "nohup sh $SPATH &> /dev/null &")
-    ssh ${SSH_OPTS} xnet@$1 "${CMD}"
+    local CMD=$(echo "nohup bash $SPATH &> /dev/null &")
+    ssh -q ${SSH_OPTS} xnet@$1 "${CMD}"
   fi
 }
 
@@ -39,7 +49,7 @@ setup_res()
     SERV=server-$i
 
     CMD="wget --no-cache -O ${ZIP} ${RES} ${SILENT}; unzip -o ${ZIP} ${SILENT}"
-    ssh ${SSH_OPTS} xnet@${SERV} "${CMD}"
+    ssh -q ${SSH_OPTS} xnet@${SERV} "${CMD}"
 
   done
 
@@ -60,25 +70,28 @@ is_installed()
 }
 
 
-# first node is the master
+# First node is the master
 NODES=('server-1' 'server-2' 'server-3' 'server-4')
 
 #Installation des dépendances
-function install_deps {
+install_deps()
+{
     #Si on est entrain de configurer le master on ajoute son hostname aux arguments du script
     #Le master requiert un peu plus de config, notamment pour spark
     if [[ $1 -eq ${NODES[0]} ]]; then
-      ssh -q -i ~/.ssh/xnet $1 "bash -s > /dev/null 2>&1" < ./install_node_deps.sh $1
+      ssh -q ${SSH_OPTS} $1 "bash -s > /dev/null 2>&1" < ./install_node_deps.sh $1
     else
-      ssh -q -i ~/.ssh/xnet $1 "bash -s > /dev/null 2>&1" < ./install_node_deps.sh
+      ssh -q ${SSH_OPTS} $1 "bash -s > /dev/null 2>&1" < ./install_node_deps.sh
     fi
 }
 
-function configure_mesos_master {
-  ssh -q -i ~/.ssh/xnet $1 "bash -s > /dev/null 2>&1" < ./configure_mesos_master.sh &
+configure_mesos_master()
+{
+  ssh -q ${SSH_OPTS} "bash -s > /dev/null 2>&1" < ./configure_mesos_master.sh &
 }
 
-function configure_mesos_slave {
-  ssh -q -i ~/.ssh/xnet $1 "bash -s > /dev/null 2>&1" < ./configure_mesos_slave.sh &
+configure_mesos_slave()
+{
+  ssh -q ${SSH_OPTS} $1 "bash -s > /dev/null 2>&1" < ./configure_mesos_slave.sh &
 }
 
