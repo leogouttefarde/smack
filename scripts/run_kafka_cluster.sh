d954428a687069306615e9590ffb3b9399712e78
OthmaneN <nahyl.othmane@gmail.com>
2016-12-06 02:03:55 +0100

    added kafka cluster run script

---
+++ scripts/run_kafka_cluster.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DIR=$(cd "$(dirname "$0")" && pwd)
+. "$DIR"/utility.sh
+
+#Lancement du scheduler kafka
+remote_run ${MASTER} "cd ~/kafka && ./kafka-mesos.sh scheduler"
+
+sleep 5
+
+#Lancement des brokers
+remote_run_sync ${MASTER} "cd ~/kafka && ./kafka-mesos.sh broker add 1..$NBR_KAFKA_BROKERS --mem 256 --heap 256"
+remote_run_sync ${MASTER} "cd ~/kafka && ./kafka-mesos.sh broker start 1..$NBR_KAFKA_BROKERS"
