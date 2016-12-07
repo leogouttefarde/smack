1e7a56619a9e4074c6ca2188e076cdb55214de5f
OthmaneN <nahyl.othmane@gmail.com>
2016-12-07 21:09:22 +0100

    fixed typo

--- scripts/run_mesos_master.sh
+++ scripts/run_mesos_master.sh
@@ -3,4 +3,4 @@
 DIR=$(cd "$(dirname "$0")" && pwd)
 . "$DIR"/utility.sh
 
-sudo service zookeeper restart && -master --zk=zk://${MASTER}:2181/mesos --quorum=1 --ip=${MY_IP} --work_dir=/var/lib/mesos > /dev/null 2>&1
+sudo service zookeeper start && sudo master --zk=zk://${MASTER}:2181/mesos --quorum=1 --ip=${MY_IP} --work_dir=/var/lib/mesos > /dev/null 2>&1
