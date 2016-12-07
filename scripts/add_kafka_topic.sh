bea6608f0f25f910d997096a009c77d77b19825d
OthmaneN <nahyl.othmane@gmail.com>
2016-12-07 19:41:41 +0100

    added basic kafka topic creation

---
+++ scripts/add_kafka_topic.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DIR=$(cd "$(dirname "$0")" && pwd)
+. "$DIR"/utility.sh
+
+echo 'Available brokers'
+remote_run_sync ${MASTER} "cd ~/kafka && ./kafka-mesos.sh broker start list"
+
+echo 'Enter the broker id: '
+read BROKER
+
+echo 'Enter the name of the topic: '
+read TOPIC_NAME
+
+if [[ ${BROKER} -ne '' && ${TOPIC_NAME} -ne '' ]];then
+    remote_run_sync ${MASTER} "cd ~/kafka && ./kafka-mesos.sh topic add $TOPIC_NAME --broker $BROKER"
+
+else
+    echo 'Invalid input. Aborting ...'
+fi
+
+
