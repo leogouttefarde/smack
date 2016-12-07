373a9128cf609a8d2d5d67fa28aab6e8ecff6bfc
Philippe Boudouin <philippe.boudouin@ensimag.grenoble-inp.fr>
2016-12-07 09:46:10 +0100

    Renommage et réorganisation des fichiers de test kafka

--- Kafka_test/Kafka_Test_Producer/src/TestProducer.java
+++ Kafka_test/Kafka_Test_Producer/src/TestProducer.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 //How to use : 
 //Start up a kafka cluster on localhost, create a topic named test, and run the ConsumerTest and the ProducerTest in this order.
 //TODO : use on distant servers
-public class Test
+public class TestProducer
 {
 	public static void main(String[] args) throws IOException {
 	     	ProducerTest testProd = new ProducerTest();
