2b658a9ad5f8a28b0e89d05722f9860a77847bf0
Philippe Boudouin <philippe.boudouin@ensimag.grenoble-inp.fr>
2016-12-07 10:31:18 +0100

    Makefile de compilation du test Kafka

--- Kafka_test/Kafka_Test_Producer/src/TestProducerMain.java
+++ Kafka_test/Kafka_Test_Producer/src/TestProducerMain.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 //How to use : 
 //Start up a kafka cluster on localhost, create a topic named test, and run the ConsumerTest and the ProducerTest in this order.
 //TODO : use on distant servers
-public class TestProducer
+public class TestProducerMain
 {
 	public static void main(String[] args) throws IOException {
 	     	ProducerTest testProd = new ProducerTest();
