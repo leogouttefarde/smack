355f9aa4e1a616542514d23da9cd144ee5bffe93
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2016-12-07 17:00:07 +0100

    Fix install oracle

--- scripts/install_cassandra.sh
+++ scripts/install_cassandra.sh
@@ -25,9 +25,11 @@ sudo make altinstall &>/dev/null
 echo "Installation de java (oracle) sur $SELF"
 
 cd ~/
-sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-#sudo apt-get -y install oracle-java8-installer
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get -y install oracle-java8-installer
 
 
 # Installation de CASSANDRA
