bce7c911372cb12b6e47441d37e59c65360df940
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2016-12-07 03:19:55 +0100

    fix

--- scripts/install_cassandra.sh
+++ scripts/install_cassandra.sh
@@ -27,7 +27,7 @@ echo "Installation de java (oracle) sur $SELF"
 cd ~/
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get -y install oracle-java8-installer
+#sudo apt-get -y install oracle-java8-installer
 
 
 # Installation de CASSANDRA
