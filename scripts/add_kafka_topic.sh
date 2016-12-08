e1b4fc97d2666cb48f64133b7a696e23ec0663e4
OthmaneN <nahyl.othmane@gmail.com>
2016-12-08 20:26:11 +0100

    kafka add topic script fix

--- scripts/add_kafka_topic.sh
+++ scripts/add_kafka_topic.sh
@@ -12,7 +12,7 @@ read BROKER
 echo 'Enter the name of the topic: '
 read TOPIC_NAME
 
-if [[ ${BROKER} -ne '' && ${TOPIC_NAME} -ne '' ]];then
+if [ ${BROKER} -ne '' ] && [ ${TOPIC_NAME} -ne '' ];then
     remote_run_sync ${MASTER} "cd ~/kafka && ./kafka-mesos.sh topic add $TOPIC_NAME --broker $BROKER"
 
 else
