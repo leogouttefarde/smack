8f9f51b3541a54a9d4bf929e2fb3d1cf78a88b74
OthmaneN <nahyl.othmane@gmail.com>
2016-12-08 16:23:40 +0100

    zookeper fix

--- scripts/run_mesos_master.sh
+++ scripts/run_mesos_master.sh
@@ -3,4 +3,4 @@
 DIR=$(cd "$(dirname "$0")" && pwd)
 . "$DIR"/utility.sh
 
-sudo service zookeeper start && sudo mesos-master --zk=zk://${MASTER}:2181/mesos --quorum=1 --ip=${MY_IP} --work_dir=/var/lib/mesos > /dev/null 2>&1
+sudo service zookeeper restart && sudo mesos-master --zk=zk://${MASTER}:2181/mesos --quorum=1 --ip=${MY_IP} --work_dir=/var/lib/mesos > /dev/null 2>&1
