3b7912320a8f6ea169cc78a1679de915845f2fb7
OthmaneN <nahyl.othmane@gmail.com>
2016-12-29 14:52:22 +0100

    fixed quorum size

--- scripts/run_mesos_master.sh
+++ scripts/run_mesos_master.sh
@@ -3,4 +3,4 @@
 DIR=$(cd "$(dirname "$0")" && pwd)
 . "$DIR"/utility.sh
 
-sudo service zookeeper restart && sudo mesos-master --zk=zk://${JOINED_MASTERS_WITH_ZK_PORT}/mesos --quorum=3 --ip=${MY_IP} --work_dir=/var/lib/mesos > /dev/null 2>&1
+sudo service zookeeper restart && sudo mesos-master --zk=zk://${JOINED_MASTERS_WITH_ZK_PORT}/mesos --quorum=2 --ip=${MY_IP} --work_dir=/var/lib/mesos > /dev/null 2>&1
