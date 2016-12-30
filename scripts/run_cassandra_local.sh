9735b09f79daec3e6a4b7c79978c444b808b08f0
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2016-12-30 23:05:00 +0100

    +

--- scripts/run_cassandra_local.sh
+++ scripts/run_cassandra_local.sh
@@ -7,7 +7,8 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 export JVM_OPTS="$JVM_OPTS Xss256k -Xms64m -Xmx256m"
 
 echo "Running Cassandra on $SELF"
-echo "PIDF_CASSANDRA = $PIDF_CASSANDRA"
+# echo "PIDF_CASSANDRA = $PIDF_CASSANDRA"
 
-sudo ~/apache-cassandra-3.9/bin/cassandra -f -R -p ${PIDF_CASSANDRA} > cassandra.log &
+sudo ~/apache-cassandra-3.9/bin/cassandra -f -R > cassandra.log &
+# sudo ~/apache-cassandra-3.9/bin/cassandra -f -R -p ${PIDF_CASSANDRA} > cassandra.log &
 
