f905b19beb68cab9fa61cfe1b5469861adbae55e
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2016-12-31 21:57:54 +0100

    +

--- scripts/run_cassandra_local.sh
+++ scripts/run_cassandra_local.sh
@@ -3,16 +3,15 @@
 DIR=$(cd "$(dirname "$0")" && pwd)
 . "$DIR"/utility.sh
 
-# If Cassandra node currently down, replace it
-if ~/apache-cassandra-3.9/bin/nodetool status | grep $MY_IP | grep UN; then
-  DEAD=0
-else
-  DEAD=1
-  CENV=~/apache-cassandra-3.9/conf/cassandra-env.sh
 
-  cp -f $CENV $CENV.old
-  echo 'JVM_OPTS="$JVM_OPTS -Dcassandra.replace_address='$MY_IP'"' >> $CENV
-fi
+echo "Running Cassandra on $SELF"
+
+
+DEAD=1
+CENV=~/apache-cassandra-3.9/conf/cassandra-env.sh
+
+cp -f $CENV $CENV.old
+echo 'JVM_OPTS="$JVM_OPTS -Dcassandra.replace_address='$MY_IP'"' >> $CENV
 
 
 
@@ -22,8 +21,6 @@ fi
 
 # export JVM_OPTS="$JVM_OPTS Xss256k -Xms64m -Xmx256m"
 export JVM_OPTS="$JVM_OPTS Xss1024k -Xms64m -Xmx512m"
-
-echo "Running Cassandra on $SELF"
 
 sudo $XNET/apache-cassandra-3.9/bin/cassandra -R -p ${PIDF_CASSANDRA} > $XNET/cassandra.log
 
