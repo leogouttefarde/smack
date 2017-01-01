91e182bb4e82eaa70a4cd942eaf0f4373e1c6c6e
OthmaneN <nahyl.othmane@gmail.com>
2017-01-01 16:21:50 +0100

    added creation of mesos pid file

--- scripts/run_mesos_slave.sh
+++ scripts/run_mesos_slave.sh
@@ -4,3 +4,4 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 . "$DIR"/utility.sh
 
 sudo mesos-slave --ip=${MY_IP} --hostname=${MY_IP} --master=zk://${JOINED_MASTERS_WITH_ZK_PORT}/mesos --work_dir=/var/lib/mesos --log_dir=/var/log/mesos > /dev/null 2>&1
+sudo lsof -t -i:5051 > $PIDF_MESOS
