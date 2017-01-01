fbf4d70e47af93bc48ea7319536d943cb3fbe82b
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2017-01-01 18:48:03 +0100

    Fixed latest mesos monit scripts ++

--- scripts/kill_mesos_processes.sh
+++ scripts/kill_mesos_processes.sh
@@ -4,12 +4,12 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 . "$DIR"/utility.sh
 
 
-SELF=$(hostname)
+if array_contains SLAVES ${SELF}; then
 
-if array_contains SLAVES ${SELF}
-then
-    ~/kill_mesos_local.sh
-elif array_contains MASTERS ${SELF}
-then
-    ~/kill_mesos_local.sh im_a_master
+  $XNET/scripts/kill_mesos_local.sh
+
+elif array_contains MASTERS ${SELF}; then
+
+  $XNET/scripts/kill_mesos_local.sh im_a_master
+
 fi
--- scripts/run_mesos_local.sh
+++ scripts/run_mesos_local.sh
@@ -4,13 +4,13 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 . "$DIR"/utility.sh
 
 
-SELF=$(hostname)
+if array_contains SLAVES ${SELF}; then
 
-if array_contains SLAVES ${SELF}
-then
-    ~/run_mesos_slave.sh
-elif array_contains MASTERS ${SELF}
-then
-    ~/run_mesos_master.sh
+  $XNET/scripts/run_mesos_slave.sh
+
+elif array_contains MASTERS ${SELF}; then
+
+  $XNET/scripts/run_mesos_master.sh
+
 fi
 
