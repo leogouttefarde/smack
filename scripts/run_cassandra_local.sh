c965c1e6e5bf0a0abc041364d78d4eee3c7b85a7
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2017-01-01 05:58:56 +0100

    Optim

--- scripts/run_cassandra_local.sh
+++ scripts/run_cassandra_local.sh
@@ -11,17 +11,32 @@ DEAD=0
 # TODO : il faudrait ne mettre DEAD à 1 que quand on remplace un noeud qui est down, mais comment le savoir ?
 # En effet quand le noeud est down nodetool n'est plus actif donc il faudrait demander à des noeuds fonctionnels
 # Pour le moment on va passer en mode remplacement uniquement si un argument est fourni.
-if [[ $# -eq 1 ]]; then
-  DEAD=1
-fi
+# if [[ $# -eq 1 ]]; then
+#   DEAD=1
+# fi
 
-if [[ $# -ge 2 ]]; then
-  if (( RANDOM % 2 )); then
-    DEAD=1;
-  fi
-fi
+# On regarde si un noeud cassandra nous connaît,
+# si oui alors on doit remplacer le processus mort
+# si non on doit démarrer normalement
+for SERV in "${NODES[@]}"; do
+
+    if remote_run_sync "~/scripts/cassandra_status.sh | grep $MY_IP"; then
+      DEAD=1;
+      break;
+    fi
+
+done
+
+# if [[ $# -ge 2 ]]; then
+#   if (( RANDOM % 2 )); then
+#     DEAD=1;
+#   fi
+# fi
+
 
 if [ $DEAD -eq 1 ]; then
+
+  echo "Replacing dead node"
 
   CENV=$XNET/apache-cassandra-3.9/conf/cassandra-env.sh
 
@@ -29,6 +44,10 @@ if [ $DEAD -eq 1 ]; then
   chmod $CENV 666
   echo 'JVM_OPTS="$JVM_OPTS -Dcassandra.replace_address='$MY_IP'"' >> $CENV
   chmod $CENV 644
+
+else
+
+  echo "Starting node"
 
 fi
 
@@ -46,7 +65,7 @@ sudo $XNET/apache-cassandra-3.9/bin/cassandra -R -p ${PIDF_CASSANDRA} > $XNET/ca
 
 
 # If Cassandra node currently down, finish repairing
-if [ "$DEAD" -eq "1" ]; then
+if [ $DEAD -eq 1 ]; then
 
   $XNET/apache-cassandra-3.9/bin/nodetool repair
   mv -f $CENV.old $CENV
