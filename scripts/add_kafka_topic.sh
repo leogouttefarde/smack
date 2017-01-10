29d35c17f152cede334afe12d00b30bda23d578c
OthmaneN <OthmaneN@users.noreply.github.com>
2017-01-10 20:17:39 +0100

    Fixed topic script

--- scripts/add_kafka_topic.sh
+++ scripts/add_kafka_topic.sh
@@ -4,7 +4,7 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 . "$DIR"/utility.sh
 
 echo 'Available brokers'
-remote_run_sync ${MASTER} "cd ~/kafka && ./kafka-mesos.sh broker list"
+cd ~/kafka && ./kafka-mesos.sh broker list
 
 echo 'Enter the broker id: '
 read BROKER
@@ -13,7 +13,7 @@ echo 'Enter the name of the topic: '
 read TOPIC_NAME
 
 if [[ ${BROKER} != '' ]] && [[ ${TOPIC_NAME} != '' ]];then
-    remote_run_sync ${MASTER} "cd ~/kafka && ./kafka-mesos.sh topic add $TOPIC_NAME --broker $BROKER"
+    cd ~/kafka && ./kafka-mesos.sh topic add $TOPIC_NAME --broker $BROKER
 else
     echo 'Invalid input. Aborting ...'
 fi
