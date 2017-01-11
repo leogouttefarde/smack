9e0e2dc49714dfe73a99016d4c82343ee5ff7303
edoh <edoh@localhost.localdomain>
2017-01-11 07:22:56 +0000

    data integration using kakfa

--- newfolder/cassandra_status.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# Runs commands on all servers
-
-DIR=$(cd "$(dirname "$0")" && pwd)
-. "$DIR"/utility.sh
-
-
-~/apache-cassandra-3.9/bin/nodetool status
--- newfolder/update_setup.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# Updates the setup files everywhere
-
-DIR=$(cd "$(dirname "$0")" && pwd)
-. "$DIR"/utility.sh
-
-setup_res
-
