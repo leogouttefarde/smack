0398bf623aee89d5189201e49128a558f70d2f15
OthmaneN <nahyl.othmane@gmail.com>
2017-01-19 13:55:38 +0100

    Fix monit install

--- scripts/install_mmonit.sh
+++ scripts/install_mmonit.sh
@@ -11,20 +11,4 @@ sudo service monit stop
 sudo apt-get install monit html2text -y
 
 MONITVER=$(wget -q https://mmonit.com/monit/dist/binary/ -O - | html2text | grep DIR | tail -n 1 | tr -d / | awk '{print $2}')
-cd /tmp
-wget https://mmonit.com/monit/dist/binary/$MONITVER/monit-$MONITVER-linux-x64.tar.gz 2>/dev/null
-tar -xf monit-*.tar.gz
-rm -f monit-*.tar.gz
-cd monit-*
-sudo rm -f /usr/bin/monit
-sudo cp -f bin/monit /usr/bin/moni
-sudo ln -s /etc/monit/monitrc /etc/monitrc
-sudo cp -f /etc/monit/monitrc{,.old}
-sudo service monit restart
-cd ~
-wget https://mmonit.com/dist/mmonit-3.6.2-linux-x64.tar.gz -O mmonit.tgz 2>/dev/null
-tar -xzf mmonit.tgz
-rm -f mmonit.tgz
-
-
-
+ cd /tmp && wget https://mmonit.com/monit/dist/binary/$MONITVER/monit-$MONITVER-linux-x64.tar.gz 2>/dev/null && tar -xf monit-*.tar.gz && rm -f monit-*.tar.gz && cd monit-* && sudo rm -f /usr/bin/monit && sudo cp -f bin/monit /usr/bin/moni && sudo ln -s /etc/monit/monitrc /etc/monitrc && sudo cp -f /etc/monit/monitrc{,.old} && sudo service monit restart && cd ~ && wget https://mmonit.com/dist/mmonit-3.6.2-linux-x64.tar.gz -O mmonit.tgz 2>/dev/null && tar -xzf mmonit.tgz && rm -f mmonit.tgz
