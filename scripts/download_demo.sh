5758b851792506cad0fc4401230dd571333ad026
OthmaneN <nahyl.othmane@gmail.com>
2017-01-20 04:52:45 +0100

    Added demo download script

---
+++ scripts/download_demo.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+wget -O ~/apps_demo.zip https://github.com/leogouttefarde/smack/blob/master/apps_demo.zip?raw=true
+wget -O ~/test-small.csv https://github.com/leogouttefarde/smack/blob/master/test-small.csv?raw=true
+
+unzip ~/apps_demo.zip -d /home/xnet
